b491d9c5ccad216280c6eda9d8111c71f63c7532
Alexankitty <alexankitty@gmail.com>
2023-07-14 19:55:13 -0600

    Fixed font processing

--- imageProcessor.py
+++ imageProcessor.py
@@ -1,59 +1,93 @@
 from PIL import Image
 
 class Font:
-    def __init__(self, imagePath, size, kerning, empty, template):
+    def __init__(self, imagePath, size, kerning, empty, space, spaceCoord, solidBehavior, template):
         self.image = Image.open(imagePath)
         self.glyphSize = size
         self.kerning = kerning
         self.template = template
         self.width, self.height = self.image.size
+        if not empty:
+            self.empty = 0
+        else:
+            self.empty = self.glyphSize // 2
+        if not space:
+            self.spaceSize = 0
+        else:
+            self.spaceSize = space // 2
+        if spaceCoord:
+            string = spaceCoord.split(",")
+            self.spaceX = int(string[0]) // self.glyphSize
+            self.spaceY = int(string[1]) // self.glyphSize
+            self.space = True
+        if solidBehavior:
+            self.solidBehavior = True
+        else:
+            self.solidBehavior = False
         if self.width % self.glyphSize or self.height % self.glyphSize:
             raise Exception("""Image width and height must be equally divisible by the glyph size!\n
                             Image size: X:%s Y:%s Provided Glyph Size: %spx""" % (self.width, self.height, self.glyphSize))
         self.glyphColumns = self.width // self.glyphSize
         self.glyphRows = self.height // self.glyphSize
         self.refColor = self.image.getpixel((0,0))
-        self.cuts = [[None for col in range(self.glyphColumns)] for row in range(self.glyphRows)]
-        self.empty = empty // 2
-      
-        def cutGlyphs():
-            for y in range(self.glyphRows):
-                for x in range(self.glyphColumns):
-                    #Variable clean up
-                    if cutPosL:
-                        del cutPosL
-                    if cutPosR:
-                        del cutPosR
-                    segmentX = self.glyphSize * x
-                    segmentY = self.glyphSize * y
-                    for y2 in range(self.glyphSize):
-                        pixelY = segmentY + y2
-                        for x2 in range(self.glyphSize):
-                            pixelX = segmentX + x2
-                            pixelColor = self.image.getpixel(pixelX,pixelY)
-                            if isinstance(pixelColor, tuple):
-                                for z in range(len(pixelColor)):
-                                    if pixelColor[z] > self.refColor[z]:
-                                        cutPosL = (pixelX, pixelY)
-                                        if cutPosL:
-                                            cutPosR = (pixelX, pixelY)
-                            elif pixelColor > self.refColor:
-                                cutPosL = (pixelX, pixelY)
-                                if cutPosL:
-                                    cutPosR = (pixelX, pixelY)
-                if not cutPosL or cutPosR:
-                    center = self.glyphSize // 2
-                    print("Couldn't find a cut for glyph segment: X:%s Y:%s. Using the empty value." % (x, y))
-                    self.cuts[x][y] = (center - self.empty, center + self.empty)
-                self.cuts[x][y] = (cutPosL - kerning - 1 , cutPosR + kerning + 1)
-            return self.cuts
+        self.cuts = [[None for cols in range(self.glyphColumns)] for rows in range(self.glyphRows)]
 
-        def processCuts():
-            processed = ""
-            #Default processing if we don't have a template
-            if not self.template:
-                for row in range(len(self.cuts)):
-                    for col in range(len(self.cuts[row])):
-                        processed += "(%s,%s)" % self.cuts[row][col]
-
-            return processed
+        print (len(self.cuts), len(self.cuts[0]))
+    
+    def cutGlyphs(self):
+        for row in range(self.glyphRows):
+            for col in range(self.glyphColumns):
+                #Variable clean up
+                cutPosL = None
+                cutPosR = None
+                segmentX = self.glyphSize * col
+                segmentY = self.glyphSize * row
+                for x in range(self.glyphSize):
+                    pixelX = segmentX + x
+                    for y in range(self.glyphSize):
+                        pixelY = segmentY + y
+                        pixelColor = self.image.getpixel((pixelX,pixelY))
+                        if isinstance(pixelColor, tuple):
+                            for z in range(len(pixelColor)):
+                                if pixelColor[z] > self.refColor[z]:
+                                    if cutPosL == None:
+                                        cutPosL = x
+                                    if cutPosL >= 0:
+                                        cutPosR = self.glyphSize - 1 - x
+                        elif pixelColor > self.refColor:
+                            if cutPosL == None:
+                                cutPosL = x
+                            if cutPosL >= 0:
+                                cutPosR = self.glyphSize - 1 - x
+                center = self.glyphSize // 2
+                if self.space == True and row == self.spaceX and col == self.spaceY:
+                    self.cuts[row][col] = (center - self.spaceSize, center + self.spaceSize)
+                elif cutPosL == None and cutPosR == None:
+                    if center == self.empty:
+                        print("Couldn't find a cut for glyph segment: X:%s Y:%s. Using %s." % (row, col, 0))
+                        self.cuts[row][col] = (0,0)
+                    else:
+                        print("Couldn't find a cut for glyph segment: X:%s Y:%s. Using %s from center." % (row, col, self.empty))
+                        self.cuts[row][col] = (center - self.empty, center + self.empty)
+                elif cutPosL == 0 and cutPosR == 0:
+                    if self.solidBehavior:
+                        print("Reached end of glyphs, not processing more.")
+                        return self.cuts
+                    self.cuts[row][col] = (0,0)
+                else:
+                    self.cuts[row][col] = (cutPosL - self.kerning, cutPosR - self.kerning)
+        return self.cuts
+    
+    def processCuts(self):
+        processed = ""
+        #Default processing if we don't have a template
+        if not self.template:
+            for row in range(len(self.cuts)):
+                if self.cuts[row] == None:
+                    return processed
+                for col in range(len(self.cuts[row])):
+                    if self.cuts[row][col] == None:
+                        return processed
+                    processed += "(%s,%s)," % self.cuts[row][col]
+                processed += "\n"
+        return processed
--- main.py
+++ main.py
@@ -7,8 +7,7 @@ parser = argparse.ArgumentParser(
     prog='Glyph Cutter 9000',
     description='Cuts bitmap glyphs to a template type of your choosing.',
     epilog='Written by Alexankitty 2023.',
-    add_help=False
-)
+    add_help=False)
 
 requiredArgs = parser.add_argument_group('required arguments')
 optionalArgs = parser.add_argument_group('optional arguments')
@@ -31,21 +30,33 @@ requiredArgs.add_argument(
     dest='kerningSize',
     required=True)
 requiredArgs.add_argument(
-    '-e','--empty',
-    type=int,
-    help='Amount of space (in px) for glyphs where a cut cannot be found.',
-    dest='emptySize',
-    required=True)
-requiredArgs.add_argument(
     '-o','--output',
     help='Where to save the file once done.',
     dest='outputFile',
     required=True)
 optionalArgs.add_argument(
+    '-sp','--space',
+    type=int,
+    help='Size (in px) of the space character.',
+    dest='spaceSize')
+optionalArgs.add_argument(
+    '-c','--space-coordinate',
+    help="Coordinates of a pixel in the space glyph. I'll do the math for you to find the glyph row and column. Format: x,y",
+    dest='spaceCoord')
+optionalArgs.add_argument(
+    '-e','--empty',
+    type=int,
+    help='Amount of space (in px) for glyphs where a cut cannot be found.',
+    dest='emptySize')
+optionalArgs.add_argument(
+    '-T','--treat-solid-as-end',
+    help='Treats any completely solid glyphs as the end of the glyph set.',
+    dest='solidBehavior',
+    action='store_true')
+optionalArgs.add_argument(
     '-t','--templatefile',
     dest='templateFile',
-    help='Path to a template text file to tell this program how to build the cuts into the output. Not including a template will put the cuts directly into a txt file.'
-    )
+    help='Path to a template text file to tell this program how to build the cuts into the output. Not including a template will put the cuts directly into a txt file.')
 optionalArgs.add_argument(
     '-g','--gui',
     help='Display the GUI version for inputting these arguments.',
@@ -55,21 +66,27 @@ optionalArgs.add_argument(
     help='Show this help message and exit',
     action='help')
 
+args = parser.parse_args()
 if len(sys.argv) < 2:
     parser.print_help()
-    print("\n")
 
 # To do: Call GUI here before we dive into converting to globals.
 
 # Convert arguments into globals.
-
 if len(sys.argv) > 3:
-    args = parser.parse_args()
     imageFile = args.imageFile
     glyphSize = args.glyphSize
     kerningSize = args.kerningSize
+    emptySize = args.emptySize
     outputFile = args.outputFile
+    space = args.spaceSize
+    spaceCoord = args.spaceCoord
+    solidBehavior = args.solidBehavior
     templateFile = args.templateFile
 
-# Do the things.
-image = imageProcessor.loadImage(imageFile)
+font = imageProcessor.Font(imageFile, glyphSize, kerningSize, emptySize, space, spaceCoord, solidBehavior, templateFile)
+cuts = font.cutGlyphs()
+process = font.processCuts()
+output = open(outputFile, 'w')
+output.write(process)
+print(process)
